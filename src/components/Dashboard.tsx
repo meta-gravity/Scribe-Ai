5c63480686daa3774b08987febe38825f2814602
metagravity <metagravity0@gmail.com>
2023-10-03 07:39:45 +0100

    start dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,16 @@
+const Dashboard = () => {
+    return (
+        <main className="mx-auto max-w-7 md:p-10">
+            <div className="mt-8 mx-4 border-x-9 flex flex-col gap-4">
+                <div>
+                    My dashboard
+                </div>
+
+                
+                
+            </div>
+        </main>
+    )
+}
+
+export default Dashboard
