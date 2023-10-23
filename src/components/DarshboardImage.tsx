98cccb6907d9b32be14527f537607e8111e45d4a
metagravity <metagravity0@gmail.com>
2023-10-24 00:26:50 +0100

    ‖

--- src/components/DarshboardImage.tsx
+++ src/components/DarshboardImage.tsx
@@ -5,15 +5,17 @@ import da from '../../public/da.png'
 
 export function DashImage() {
   return (
-    <div className='mx-auto max-w-6xl px-6 lg:px-8'>
-      <div className='mt-16 flow-root sm:mt-24 p-2'>
-        <Image 
-          src={da} 
-          alt="me in this working" 
-          quality={100}
-          loading='lazy'
-          className='border-2 rounded-4xl'
-        />
+    <div className='bg-purple-500'>
+      <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+        <div className='mt-16 flow-root sm:mt-24 p-2'>
+          <Image 
+            src={da} 
+            alt="me in this working" 
+            quality={100}
+            loading='lazy'
+            className='rounded-4xl'
+          />
+        </div>
       </div>
     </div>
   )
