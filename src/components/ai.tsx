5890817a3607e962e729391524785a4f6e1f84ee
metagravity <metagravity0@gmail.com>
2023-12-04 07:20:41 +0100

    .n

--- src/components/ai.tsx
+++ src/components/ai.tsx
@@ -1,50 +1,50 @@
-import { Button } from './ui/button';
-import { Input } from './ui/input';
-import { SparklesIcon } from 'lucide-react';
-import { useCompletion } from 'ai/react';
+// import { Button } from './ui/button';
+// import { Input } from './ui/input';
+// import { SparklesIcon } from 'lucide-react';
+// import { useCompletion } from 'ai/react';
  
-export default function Completion() {
-  const {
-    completion,
-    input,
-    stop,
-    isLoading,
-    handleInputChange,
-    handleSubmit,
-  } = useCompletion({
-    api: '/api/completion',
-  });
+// export default function Completion() {
+//   const {
+//     completion,
+//     input,
+//     stop,
+//     isLoading,
+//     handleInputChange,
+//     handleSubmit,
+//   } = useCompletion({
+//     api: '/api/completion',
+//   });
  
-  return (
-    <div className='mx-auto w-full max-w-md py-24 flex flex-col stretch'>
-      <div className='bg-gray-500 border border-md rounded-md p-2 '> 
-        <output>{completion}</output>
-      </div>
+//   return (
+//     <div className='mx-auto w-full max-w-md py-24 flex flex-col stretch'>
+//       <div className='bg-gray-500 border border-md rounded-md p-2 '> 
+//         <output>{completion}</output>
+//       </div>
 
-      <div className='bg-gray-300 rounded border-neutral-700 border w-full max-w-md'>
+//       <div className='bg-gray-300 rounded border-neutral-700 border w-full max-w-md'>
 
-        <form onSubmit={handleSubmit} className="flex items-center gap-3 mb-8">
-          <label className="grow">
-            <Input
-              className=" border border-gray-300 rounded shadow-xl p-2"
-              value={input}
-              onChange={handleInputChange}
-              placeholder="write a blog post about dalle-3..."
-            />
-          </label>
-          <Button type="button"  onClick={stop}>
-            Stop
-          </Button>
-          <Button disabled={isLoading} type="submit">
-            <SparklesIcon className='h-5 w-5'/>
-            Send
-          </Button>
-        </form>
-      </div>
+//         <form onSubmit={handleSubmit} className="flex items-center gap-3 mb-8">
+//           <label className="grow">
+//             <Input
+//               className=" border border-gray-300 rounded shadow-xl p-2"
+//               value={input}
+//               onChange={handleInputChange}
+//               placeholder="write a blog post about dalle-3..."
+//             />
+//           </label>
+//           <Button type="button"  onClick={stop}>
+//             Stop
+//           </Button>
+//           <Button disabled={isLoading} type="submit">
+//             <SparklesIcon className='h-5 w-5'/>
+//             Send
+//           </Button>
+//         </form>
+//       </div>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
 // <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
       // <form onSubmit={handleSubmit} className="flex items-center gap-3 mb-8">
