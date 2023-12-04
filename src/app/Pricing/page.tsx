776a4132502fccb3110c12ac9c929d464ddfab11
metagravity <metagravity0@gmail.com>
2023-12-04 12:52:39 +0100

    🏢⚒️more work to be dome on pricing

--- src/app/Pricing/page.tsx
+++ src/app/Pricing/page.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
-export default function Pricing() {
+export default function pricing() {
     return (
         <MaxWidthWrapper>
-            <h1 className="text-center align-center hover:text-primary text-gray-600 dark:text-gray-300 transition-all">Pricing coming soon</h1>
+            <h1 className="text-center align-center hover:text-primary text-gray-600 dark:text-gray-300 transition-all">
+                Pricing coming soon
+            </h1>
         </MaxWidthWrapper>
     )
 } 
