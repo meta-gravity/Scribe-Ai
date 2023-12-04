d0361aac3b1409889b08d178355c7b934b42580d
metagravity <metagravity0@gmail.com>
2023-12-04 13:32:20 +0100

    🤔price work plzz

--- src/app/Pricing/page.tsx
+++ src/app/Pricing/page.tsx
@@ -3,7 +3,7 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 export default function Page() {
     return (
         <MaxWidthWrapper>
-            <h1 className="text-center hover:text-primary text-gray-600 dark:text-gray-300 transition-all text-4xl m-24 ">
+            <h1 className="text-center hover:text-primary cursor-not-allowed text-gray-600 dark:text-gray-300 transition-all text-4xl m-24 ">
                 Pricing coming soon
             </h1>
         </MaxWidthWrapper>
