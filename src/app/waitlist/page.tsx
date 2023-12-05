bccbfe8f9fdc3f67b83e6cc1c7a4f725ab5231b9
metagravity <metagravity0@gmail.com>
2023-12-05 07:57:46 +0100

    📃Waitlist

---
+++ src/app/waitlist/page.tsx
@@ -0,0 +1,17 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+export default function page() {
+    return (
+        <div className="max-w-md mx-auto space-y-8 mt-16 pt-20" >
+            <div className="max-w-md mx-auto space-y-8">
+                <div className="text-center space-y-4">
+                    <h2 className="text-4xl font-bold">Thanks for joining our Waitlist</h2>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        we would inform you when you request is approved.
+                    </p>
+                </div>
+            </div>
+        </div>
+        )
+    }
