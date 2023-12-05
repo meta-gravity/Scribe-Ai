2948ba385e0a2893630c17d17b1cd9ec3009ef7e
metagravity <metagravity0@gmail.com>
2023-12-05 09:38:03 +0100

    😵works done to test dash

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,17 @@
-import { initTRPC } from '@trpc/server';
+import { TRPCError, initTRPC } from '@trpc/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const t = initTRPC.create();
+
+const middleware = t.middleware
+
+// const isAuth = middleware(async () => {
+//   const { getUser } = getKindeServerSession()
+//   const user = getUser()
+
+//   if (!user || !user.id) {
+//     throw new TRPCError({ code: 'UNAUTHORIZED' })
+//   }
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
